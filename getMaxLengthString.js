6664813fa3dd682436c8cf9f61b9d6c7b047dd44
Supatsorn Poonsawat <supatsorn.supa@mail.kmutt.ac.th>
2022-09-15 00:04:20 +0700

    getMax1

--- getMaxLengthString.js
+++ getMaxLengthString.js
@@ -1,6 +1,28 @@
 const { template } = require('@babel/core')
 
 function getMaxLengthString(arrayOfString) {
-  //code here
-}
+    if (arrayOfString == null) {
+        return undefined;
+      } else if (arrayOfString == undefined) {
+        return undefined;
+      } else {
+        let text = [arrayOfString[0]];
+        let text2 = arrayOfString[0];
+        for (i = 0; i < arrayOfString.length; i++) {
+          if (i + 1 < arrayOfString.length) {
+            if (text2.length < arrayOfString[i + 1].length) {
+              text2 = arrayOfString[i + 1];
+              text = [];
+              text = [arrayOfString[i + 1]];
+            } else if (text2.length == arrayOfString[i + 1].length) {
+              text.push(arrayOfString[i + 1]);
+            }
+          }
+        }
+        return text;
+      }
+  }
+
+
+
 module.exports = getMaxLengthString
